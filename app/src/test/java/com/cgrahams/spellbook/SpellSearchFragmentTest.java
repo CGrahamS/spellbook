108fc46d3103b6716ce7451a8d07189f9f98d070
Caleb Stevenson <cgrahamstevenson@gmail.com>
2016-12-21 16:55:29 -0800

    add shadow of listview

--- app/src/test/java/com/cgrahams/spellbook/SpellSearchFragmentTest.java
+++ app/src/test/java/com/cgrahams/spellbook/SpellSearchFragmentTest.java
@@ -9,12 +9,15 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowListView;
+import org.robolectric.shadows.ShadowLog;
 
 import static com.cgrahams.spellbook.support.Assert.assertViewIsVisible;
 import static com.cgrahams.spellbook.support.ResourceLocator.getString;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.*;
+import static org.robolectric.Shadows.shadowOf;
 import static org.robolectric.shadows.support.v4.SupportFragmentTestUtil.startFragment;
 
 
@@ -30,6 +33,7 @@ public class SpellSearchFragmentTest {
         startFragment(fragment);
         searchHeaderTextView = (TextView) fragment.getView().findViewById(R.id.searchFragmentHeaderTextView);
         spellSearchListView = (ListView) fragment.getView().findViewById(R.id.spellSearchListView);
+        ShadowLog.stream = System.out;
     }
 
     @Test
@@ -45,7 +49,12 @@ public class SpellSearchFragmentTest {
 
     @Test
     public void shouldHaveListView() throws Exception {
-        assertViewIsVisible(spellSearchListView);
         assertNotNull(spellSearchListView);
+        ShadowListView shadowListView = shadowOf(spellSearchListView);
+        shadowListView.populateItems();
+        shadowListView.getHeaderViews();
+        assertViewIsVisible(spellSearchListView);
     }
+
+
 }
